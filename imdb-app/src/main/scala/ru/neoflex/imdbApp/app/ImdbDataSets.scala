2af76cf9a6ce1c2d7357cc8ee8db6667ae1720e9
Alex Rook <rook.alex@gmail.com>
2025-07-14 09:32:39 +0300

    ImdbDataSets now contains all imdb dataset as lazy vals

---
+++ imdb-app/src/main/scala/ru/neoflex/imdbApp/app/ImdbDataSets.scala
@@ -0,0 +1,134 @@
+package ru.neoflex.imdbApp.app
+
+import ru.neoflex.imdbApp.models.config.AppConfig
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.execution.columnar.compression.Encoder
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.Encoder
+
+final case class ImdbDataSets(appConfig: AppConfig, spark: SparkSession) {
+  import ru.neoflex.imdbApp.models._
+
+  import spark.implicits._
+
+  def getDatasetAbsolutePath(datasetFileName: String) =
+    s"${appConfig.files.datasetDir}/$datasetFileName"
+
+  val nameBasicRowTSV: String = getDatasetAbsolutePath("name.basics.tsv")
+  val titleAkasTSV:    String = getDatasetAbsolutePath("title.akas.tsv")
+  val titleBasicsTSV:  String = getDatasetAbsolutePath("title.basics.tsv")
+  val titleCrewTSV:    String = getDatasetAbsolutePath("title.crew.tsv")
+  val titleEpisodeTSV: String = getDatasetAbsolutePath("title.episode.tsv")
+
+  val titlePrincipalsTSV: String = getDatasetAbsolutePath(
+    "title.principals.tsv"
+  )
+  val titleRatingsTSV: String = getDatasetAbsolutePath("title.ratings.tsv")
+
+  lazy val nameBasicsDataset: Dataset[NameBasicItem] =
+    readIMDBDataset[NameBasicRow, NameBasicItem](nameBasicRowTSV) {
+      case NameBasicRow(
+            nconst,
+            primaryName,
+            birthYear,
+            deathYear,
+            primaryProfession,
+            titles
+          ) =>
+        NameBasicItem(
+          nconst = nconst,
+          primaryName = primaryName,
+          birthYear = birthYear,
+          deathYear = deathYear,
+          primaryProfession = primaryProfession.asList,
+          titles = titles.asList
+        )
+    }.cache()
+
+  lazy val titleAkasDataset: Dataset[TitleAkasItem] =
+    readIMDBDataset[TitleAkasRow, TitleAkasItem](titleAkasTSV) {
+      case TitleAkasRow(
+            titleId,
+            ordering,
+            title,
+            region,
+            language,
+            types,
+            attributes,
+            isOriginalTitle
+          ) =>
+        TitleAkasItem(
+          titleId = titleId,
+          ordering = ordering,
+          title = title,
+          region = region.clean,
+          language = language.clean,
+          types = types.asList,
+          attributes = attributes.asList,
+          isOriginalTitle = isOriginalTitle.map(_ > 0)
+        )
+    }.cache()
+
+  lazy val titleBasicsDataset: Dataset[TitleBasicsItem] =
+    readIMDBDataset[TitleBasicsRow, TitleBasicsItem](titleBasicsTSV) {
+      case TitleBasicsRow(
+            tconst,
+            titleType,
+            primaryTitle,
+            originalTitle,
+            isAdult,
+            startYear,
+            endYear,
+            runtimeMinutes,
+            genres
+          ) =>
+        TitleBasicsItem(
+          tconst = tconst,
+          titleType = titleType,
+          primaryTitle = primaryTitle,
+          originalTitle = originalTitle,
+          isAdult = isAdult.map(_ > 0),
+          startYear = startYear,
+          endYear = endYear,
+          runtimeMinutes = runtimeMinutes,
+          genres = genres.asList
+        )
+    }.cache()
+
+  lazy val titleCrewDataset: Dataset[TitleCrewItem] =
+    readIMDBDataset[TitleCrewRow, TitleCrewItem](titleCrewTSV) {
+      case TitleCrewRow(tconst, directors, writers) =>
+        TitleCrewItem(
+          tconst = tconst,
+          directors = directors.asList,
+          writers = writers.asList
+        )
+    }.cache()
+
+  lazy val titleEpisodeDataset: Dataset[TitleEpisodeItem] =
+    readIMDBDataset(titleEpisodeTSV)(identity[TitleEpisodeItem]).cache()
+
+  lazy val titlePrincipalsDataset: Dataset[TitlePrincipalsItem] =
+    readIMDBDataset(titlePrincipalsTSV) { i: TitlePrincipalsItem =>
+      i.copy(
+        job = i.job.flatMap(_.clean),
+        characters = i.characters.flatMap(_.clean)
+      )
+    }.cache()
+
+  lazy val titleRatingsDataset: Dataset[TitleRatingItem] =
+    readIMDBDataset(titleRatingsTSV)(identity[TitleRatingItem]).cache()
+
+  def readIMDBDataset[R: Encoder, T: Encoder](
+    fName: String
+  )(f:     R => T): Dataset[T] =
+    spark.read
+      .format("csv")
+      .option("header", "true")
+      .option("delimiter", "\t")
+      .schema(schema = implicitly[Encoder[R]].schema)
+      .load(fName)
+      .as[R]
+      .map(f)
+
+}
