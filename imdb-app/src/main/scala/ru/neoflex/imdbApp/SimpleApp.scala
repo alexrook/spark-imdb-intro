34ea0249be33fb2ad1aad914933601180b138a3e
Alex Rook <rook.alex@gmail.com>
2025-07-14 09:32:39 +0300

    name.basic readign added

--- imdb-app/src/main/scala/ru/neoflex/imdbApp/SimpleApp.scala
+++ imdb-app/src/main/scala/ru/neoflex/imdbApp/SimpleApp.scala
@@ -1,17 +1,67 @@
 package ru.neoflex.imdbApp
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.Dataset
 
 object SimpleApp {
 
+  import org.apache.spark.sql.SparkSession
+
+  import org.apache.spark.sql.Encoder
+
+  case class NameBasicRow(
+    nconst:            String,
+    primaryName:       String,
+    birthYear:         Option[Int],
+    deathYear:         Option[Int],
+    primaryProfession: String,
+    titles:            String
+  )
+
+  case class NameBasicItem(
+    nconst:            String,
+    primaryName:       String,
+    birthYear:         Option[Int],
+    deathYear:         Option[Int],
+    primaryProfession: List[String],
+    titles:            List[String]
+  )
+
   def main(args: Array[String]): Unit = {
-    val ds1 = "file:///var/tmp/data/datasets/imdb/name.basics.tsv"
-    val sc = SparkSession.builder.appName("Simple Application").getOrCreate()
-    val nameBasicsData = sc.read.textFile(ds1).cache()
-    val numAs = nameBasicsData.filter(line => line.contains("a")).count()
-    val numBs = nameBasicsData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
-    sc.stop()
+
+    val nameBasicRowTSV: String = "file:///var/tmp/data/datasets/imdb/name.basics.tsv"
+
+    val spark: SparkSession =
+      SparkSession.builder.appName("Simple Application").getOrCreate()
+
+    import spark.implicits._
+
+    val schema = implicitly[Encoder[NameBasicRow]].schema
+
+    println(schema)
+
+    val nameBasicsDataset: Dataset[NameBasicItem] =
+      spark.read
+        .format("csv")
+        .option("header", "true")
+        .option("delimiter", "\t")
+        .schema(schema = schema)
+        .load(nameBasicRowTSV)
+        .as[NameBasicRow]
+        .map { case NameBasicRow(nconst, primaryName, birthYear, deathYear, primaryProfession, titles) =>
+          NameBasicItem(
+            nconst = nconst,
+            primaryName = primaryName,
+            birthYear = birthYear,
+            deathYear = deathYear,
+            primaryProfession = primaryProfession.replaceAll("[\\[\\]]", "").split(",").toList,
+            titles = titles.replaceAll("[\\[\\]]", "").split(",").toList
+          )
+
+        }
+
+    nameBasicsDataset.show()
+
+    spark.stop()
   }
 
 }
