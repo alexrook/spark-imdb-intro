d68c45a544fd51b7109b26887a645d077a5dba0f
Alex Rook <rook.alex@gmail.com>
2025-07-14 09:32:39 +0300

    ImdbSamplesMod titlePrincipalsSamples generation added

--- imdb-app/src/main/scala/ru/neoflex/imdbApp/app/ImdbSamplesMod.scala
+++ imdb-app/src/main/scala/ru/neoflex/imdbApp/app/ImdbSamplesMod.scala
@@ -27,7 +27,11 @@ object ImdbSamplesMod {
       SparkSession.builder.config(sparkConf).getOrCreate()
 
     val imdbDataSets: ImdbDataSets =
-      ImdbDataSets(appConfig.files.datasetDir,datasetFileEx = ".tsv", spark = spark)
+      ImdbDataSets(
+        appConfig.files.datasetDir,
+        datasetFileEx = "tsv",
+        spark = spark
+      )
 
     import spark.implicits._
 
@@ -44,7 +48,7 @@ object ImdbSamplesMod {
 
     titleRatingsSample.show(20)
 
-    write(titleRatingsSample, samplesDir, "title.rating.sample")(identity)
+    // write(titleRatingsSample, samplesDir, "title.rating.sample")(identity)
 
     //я хочу фильмы только с рейтингом
     val titlesBasicWithRating: Dataset[TitleBasicsItem] =
@@ -57,23 +61,35 @@ object ImdbSamplesMod {
 
     titlesBasicWithRating.show(20)
 
-    write(titlesBasicWithRating, samplesDir, "title.basic.sample") {
-      item: TitleBasicsItem =>
-        TitleBasicsRow(
-          tconst = item.tconst,
-          titleType = item.titleType,
-          primaryTitle = item.primaryTitle,
-          originalTitle = item.originalTitle,
-          isAdult = item.isAdult.map {
-            case true => 1
-            case _    => 0
-          },
-          startYear = item.startYear,
-          endYear = item.endYear,
-          runtimeMinutes = item.runtimeMinutes,
-          genres = item.genres.mkString(",")
+    // write(titlesBasicWithRating, samplesDir, "title.basic.sample") {
+    //   item: TitleBasicsItem =>
+    //     TitleBasicsRow(
+    //       tconst = item.tconst,
+    //       titleType = item.titleType,
+    //       primaryTitle = item.primaryTitle,
+    //       originalTitle = item.originalTitle,
+    //       isAdult = item.isAdult.map {
+    //         case true => 1
+    //         case _    => 0
+    //       },
+    //       startYear = item.startYear,
+    //       endYear = item.endYear,
+    //       runtimeMinutes = item.runtimeMinutes,
+    //       genres = item.genres.mkString(",")
+    //     )
+    // }
+
+    val titlePrincipalsSamples =
+      titlesBasicWithRating
+        .join(
+          imdbDataSets.titlePrincipalsDataset,
+          "tconst",
+          "inner"
         )
-    }
+        .as[TitlePrincipalsItem]
+        .cache()
+
+    write(titlePrincipalsSamples, samplesDir, "title.principals")(identity)
 
     println("==========Samples generation done==============")
 
