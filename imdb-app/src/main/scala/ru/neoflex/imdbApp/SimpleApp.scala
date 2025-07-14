dd442e20c895453f14c937dc4c3b116a13b88513
Alex Rook <rook.alex@gmail.com>
2025-07-14 09:32:39 +0300

    reading from imdb tsv refactored

--- imdb-app/src/main/scala/ru/neoflex/imdbApp/SimpleApp.scala
+++ imdb-app/src/main/scala/ru/neoflex/imdbApp/SimpleApp.scala
@@ -8,60 +8,63 @@ object SimpleApp {
 
   import org.apache.spark.sql.Encoder
 
-  case class NameBasicRow(
-    nconst:            String,
-    primaryName:       String,
-    birthYear:         Option[Int],
-    deathYear:         Option[Int],
-    primaryProfession: String,
-    titles:            String
-  )
-
-  case class NameBasicItem(
-    nconst:            String,
-    primaryName:       String,
-    birthYear:         Option[Int],
-    deathYear:         Option[Int],
-    primaryProfession: List[String],
-    titles:            List[String]
-  )
+  import ru.neoflex.imdbApp.models._
 
   def main(args: Array[String]): Unit = {
 
-    val nameBasicRowTSV: String = "file:///var/tmp/data/datasets/imdb/name.basics.tsv"
+    val datasetsDir = "file:///var/tmp/data/datasets/imdb"
 
-    val spark: SparkSession =
+    val nameBasicRowTSV: String = s"$datasetsDir/name.basics.tsv"
+    val titleAkasTSV:    String = s"$datasetsDir/title.akas.tsv"
+
+    implicit val spark: SparkSession =
       SparkSession.builder.appName("Simple Application").getOrCreate()
 
     import spark.implicits._
 
-    val schema = implicitly[Encoder[NameBasicRow]].schema
-
-    println(schema)
-
     val nameBasicsDataset: Dataset[NameBasicItem] =
-      spark.read
-        .format("csv")
-        .option("header", "true")
-        .option("delimiter", "\t")
-        .schema(schema = schema)
-        .load(nameBasicRowTSV)
-        .as[NameBasicRow]
-        .map { case NameBasicRow(nconst, primaryName, birthYear, deathYear, primaryProfession, titles) =>
+      readIMDBDataset[NameBasicRow, NameBasicItem](nameBasicRowTSV) {
+        case NameBasicRow(nconst, primaryName, birthYear, deathYear, primaryProfession, titles) =>
           NameBasicItem(
             nconst = nconst,
             primaryName = primaryName,
             birthYear = birthYear,
             deathYear = deathYear,
-            primaryProfession = primaryProfession.replaceAll("[\\[\\]]", "").split(",").toList,
-            titles = titles.replaceAll("[\\[\\]]", "").split(",").toList
+            primaryProfession = primaryProfession.asList,
+            titles = titles.asList
           )
+      }
 
-        }
+    val titleAkasDataset: Dataset[TitleAkasItem] =
+      readIMDBDataset[TitleAkasRow, TitleAkasItem](titleAkasTSV) {
+        case TitleAkasRow(titleId, ordering, title, region, language, types, attributes, isOriginalTitle) =>
+          TitleAkasItem(
+            titleId = titleId,
+            ordering = ordering,
+            title = title,
+            region = region.clean,
+            language = language.clean,
+            types = types.asList,
+            attributes = attributes.asList,
+            isOriginalTitle = isOriginalTitle > 0
+          )
+      }
 
     nameBasicsDataset.show()
 
+    titleAkasDataset.show()
+
     spark.stop()
   }
+
+  def readIMDBDataset[R: Encoder, T: Encoder](fName: String)(f: R => T)(implicit spark: SparkSession): Dataset[T] =
+    spark.read
+      .format("csv")
+      .option("header", "true")
+      .option("delimiter", "\t")
+      .schema(schema = implicitly[Encoder[R]].schema)
+      .load(fName)
+      .as[R]
+      .map(f)
 
 }
